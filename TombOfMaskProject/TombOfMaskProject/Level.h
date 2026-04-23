8b88796d7846aa250a52313e753c204b440039f9
Renshengwuqu <yixiangl2scd@gmail.com>
2026-04-23 21:38:27 +0200

    Update Level.h

--- TombOfMaskProject/TombOfMaskProject/Level.h
+++ TombOfMaskProject/TombOfMaskProject/Level.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 
-enum Map { Empty='-', Wall1='1', Wall2='2', Spike='S', TrapSpike='T', GunTrapTile='G', End='f' };
+enum Map { Empty='-', Wall1='1', Wall2='2', Spike='S', TrapSpike='T', GunTrapTile='G', End='f', Star='s' };
 
 class Level
 {
